b812c472f965bae0234165d5de2080ee28774ba6
Carlos <carlos@2kpixelstudios.net>
2021-04-24 17:21:38 -0400

    Added some explicit types examples

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,5 @@
 /**
- * Test description.
+ * Test description. See the difference of var vs. let vs. const [here](https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/).
  */
 var character = 'mario';
 console.log(character);
@@ -9,3 +9,50 @@ inputs.forEach(function (input, index) {
     console.log("index = " + index);
     console.log(input);
 });
+var age = 30;
+var isBlackBelt = false;
+var exampleValue = calculateCircumference(2);
+console.log(exampleValue);
+function calculateCircumference(diameter) {
+    return diameter * Math.PI;
+}
+var names = ["luigi", "mario", "yoshi"];
+names.push("Toad");
+console.log(names);
+names.splice(2, 1); //Removes 1 element at index 2 ("yoshi")
+console.log(names);
+var numbers = [10, 20, 30, 40, 50, 60];
+var mixed = ["Ken", 4, "Some data", true];
+mixed.push("Some data");
+mixed.push(10);
+mixed[2] = false;
+var ninja = {
+    name: "Ryu",
+    belt: "black",
+    age: 30
+};
+ninja.age = 40;
+ninja.age = parseInt("30");
+//Can't add new properties to it!
+// ninja.skills = []; 
+//MUST MATCH initial object structure!!
+ninja = {
+    name: "Ken",
+    belt: "orange",
+    age: 34
+};
+console.log(ninja);
+console.log(JSON.stringify(ninja));
+var emptyArray = [];
+var mixedArray = [];
+var unionMixedArray = [];
+unionMixedArray.push("David");
+unionMixedArray.push(20);
+console.log(unionMixedArray);
+var uid = "123";
+var objExample;
+objExample = { "name": "Test" };
+objExample = { "poof": "Hello?" };
+objExample = [];
+var explicitObjExample;
+// explicitObjExample = {}
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,5 @@
 /**
- * Test description.
+ * Test description. See the difference of var vs. let vs. const [here](https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/).
  */
 const character : string = 'mario';
 
@@ -12,3 +12,72 @@ inputs.forEach((input : HTMLInputElement, index : number) => {
     console.log("index = " + index);
     console.log(input);
 })
+
+let age : number = 30;
+let isBlackBelt : boolean = false;
+
+let exampleValue : number = calculateCircumference(2);
+console.log(exampleValue);
+
+function calculateCircumference(diameter : number) : number {
+    return diameter * Math.PI;
+}
+
+let names : string[] = ["luigi", "mario", "yoshi"];
+names.push("Toad");
+
+console.log(names);
+names.splice(2, 1); //Removes 1 element at index 2 ("yoshi")
+console.log(names);
+
+let numbers : number[] = [10, 20, 30, 40, 50, 60];
+
+let mixed = ["Ken", 4, "Some data", true];
+mixed.push("Some data");
+mixed.push(10);
+mixed[2] = false;
+
+let ninja = {
+    name: "Ryu",
+    belt: "black",
+    age: 30
+};
+
+ninja.age = 40;
+ninja.age = parseInt("30");
+
+//Can't add new properties to it!
+// ninja.skills = []; 
+
+//MUST MATCH initial object structure!!
+ninja = {
+    name: "Ken",
+    belt: "orange",
+    age: 34,
+    // skills: [] //CANNOT add/remove/change fields!
+};
+console.log(ninja);
+console.log(JSON.stringify(ninja));
+
+let emptyArray : string[] = [];
+let mixedArray : [] = [];
+let unionMixedArray : (string | number) [] = [];
+unionMixedArray.push("David");
+unionMixedArray.push(20);
+console.log(unionMixedArray);
+
+let uid : string | number = "123";
+
+let objExample : object;
+objExample = { "name": "Test" };
+objExample = { "poof": "Hello?" };
+objExample = [];
+
+let explicitObjExample : {
+    name: string,
+    age: number,
+    beltColor: string
+};
+// explicitObjExample = {}
+
+
